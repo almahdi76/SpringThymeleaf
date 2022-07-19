f14e0a93b79599398d771871209b94363e377702
almahdi76 <almahdi.abusef@yahoo.com>
2022-07-19 14:59:14 +0200

    ajout bootstrap

--- src/main/java/com/AKA/project/ProjectApplication.java
+++ src/main/java/com/AKA/project/ProjectApplication.java
@@ -18,7 +18,12 @@ public class ProjectApplication {
 		produitRepository.save(new Produit("Xiaomi S9",250,15));
 		produitRepository.save(new Produit("Samsung S12",1500,30));
 		produitRepository.save(new Produit("OPPO 12",450,11));
-		produitRepository.save(new Produit("Nokia 1112",110,14));
+		produitRepository.save(new Produit("Lg 60",700,90));
+		produitRepository.save(new Produit("appel S11",1100,60));
+		produitRepository.save(new Produit("Hawawi",555,33));
+		produitRepository.save(new Produit("OPPO A50",660,17));
+		produitRepository.save(new Produit("Nokia GX",800,14));
+		produitRepository.save(new Produit("Xiaom S10",220,99));
 		produitRepository.findAll().forEach(p -> System.out.println(p.getDesignation()));
 		
 		
--- src/main/java/com/AKA/project/web/ProduitController.java
+++ src/main/java/com/AKA/project/web/ProduitController.java
@@ -3,6 +3,8 @@ package com.AKA.project.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +19,13 @@ public class ProduitController {
 
 	@Autowired
 	private ProduitRepository produitRepository;
-	
-//	@RequestMapping(value="/index")
-//	public String index() {
-//		return "produits";
-//	}
+
 	
 	@RequestMapping(value="/index")
 	public String index(Model model) {
-		List<Produit> produits=produitRepository.findAll();
-		model.addAttribute("listProduit",produits);
+		List<Produit> pageProduit=
+				produitRepository.findAll();
+		model.addAttribute("listProduit",pageProduit);
 		
 		return "produits";
 	}
