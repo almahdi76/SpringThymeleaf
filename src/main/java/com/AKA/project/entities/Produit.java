e2f64757bb96eab13ab155cf3cc2e1c8e61906b5
almahdi76 <almahdi.abusef@yahoo.com>
2022-07-19 10:20:40 +0200

    creation projet spring avec generateur template thymeleaf

---
+++ src/main/java/com/AKA/project/entities/Produit.java
@@ -0,0 +1,54 @@
+package com.AKA.project.entities;
+
+import java.io.Serializable;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Produit implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String designation;
+	private double prix;
+	private int quantites;
+	public Produit() {
+		
+	}
+	public Produit(String designation, double prix, int quantites) {
+		
+		this.designation = designation;
+		this.prix = prix;
+		this.quantites = quantites;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getQuantites() {
+		return quantites;
+	}
+	public void setQuantites(int quantites) {
+		this.quantites = quantites;
+	}
+	
+	
+	
+
+}
